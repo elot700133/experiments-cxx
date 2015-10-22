e698addc3837db711c7de4cc867c8a336e49e408
Too Lee <too.lee@lmco.com>
2015-10-22 16:00:57 -0400

    multi thrad stress

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <thread>
+#include <math.h>
+#include <vector>
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
+  std::vector<thread> vt;
+  for (int i=0; i<24; i++)
+  {
+    vt.push_back( 
+        move(
+          thread( 
+            [](){
+              while(1){ sqrt(4); }
+              } 
+              )) 
+        );
+  }
+
+  for (int i=0; i<24; i++)
+  {
+    vt[i].join();
+  }
+
   
   return 0;
 }
