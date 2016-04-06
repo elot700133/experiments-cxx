8dcecfa09a1836743e1c989299db3559ffaedfc3
Too Lee <too.lee@lmco.com>
2016-04-06 17:27:55 -0400

    auto bypass private

--- main.cpp
+++ main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+class Foo
+{
+  private:
+  class Bar
+  {
+    public:
+      void print(){ printf("cat\n"); }
+  };
+  public:
+    Bar* create_bar(){ return new Bar(); }
+};
+
+
 int main(int argc, const char *argv[])
 {
-  
+  Foo foo;
+  auto bar = foo.create_bar();
+  bar->print();
+  //Foo::Bar* bar = foo.create_bar();
   return 0;
 }
