6c07dde9b5e8d152d13587034423da1a89c3eea8
Too Lee <too.lee@lmco.com>
2016-09-20 12:35:38 -0400

    start virtual func in child class

--- main.cpp
+++ main.cpp
@@ -2,8 +2,39 @@
 
 using namespace std;
 
+class Parent
+{
+  public:
+    void foo(){cout << "parent foo"<< endl;}
+};
+
+class Child: public Parent
+{
+  public:
+    virtual void foo(){cout << "child foo" << endl;}
+};
+class GrandChild: public Child
+{
+  public:
+    virtual void foo(){cout << "grand child foo" << endl;}
+};
 int main(int argc, const char *argv[])
 {
-  
+  Child c;
+  c.foo();
+
+  GrandChild gc;
+  gc.foo();
+
+
+  Parent* p = &c;
+  p->foo();
+
+  p = &gc;
+  p->foo();
+
+  Child* pc = &gc;
+  pc->foo();
+
   return 0;
 }
