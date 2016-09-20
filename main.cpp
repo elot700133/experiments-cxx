c77a2096e9ddbcfb1f4ec1918a000f99e892e262
Too Lee <too.lee@lmco.com>
2016-09-20 12:29:20 -0400

    testing detach thread

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-using namespace std;
-
-int main(int argc, const char *argv[])
+void independentThread()
 {
-  
-  return 0;
+    std::cout << "Starting concurrent thread.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+    std::cout << "Exiting concurrent thread.\n";
+}
+
+void threadCaller()
+{
+    std::cout << "Starting thread caller.\n";
+    std::thread t(independentThread);
+    t.detach();
+    t.join();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Exiting thread caller.\n";
+}
+
+int main()
+{
+    threadCaller();
+    //kjstd::this_thread::sleep_for(std::chrono::seconds(5));
 }
