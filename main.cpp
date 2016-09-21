5e570c5a831896129674c6f4768faf666bbab676
Too Lee <too.lee@lmco.com>
2016-09-21 12:09:52 -0400

    Add child class attempting to create_bar()

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ class Foo
     Bar* create_bar(){ return new Bar(); }
 };
 
+class Foo2 : public Foo
+{
+  public:
+    Bar* create_bar(){ return new Bar(); }
+};
+
 
 int main(int argc, const char *argv[])
 {
@@ -21,5 +27,8 @@ int main(int argc, const char *argv[])
   auto bar = foo.create_bar();
   bar->print();
   //Foo::Bar* bar = foo.create_bar();
+
+  Foo2 foo2;
+  foo2.create_bar(); // will not compile because Bar is private
   return 0;
 }
