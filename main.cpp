4eca28815a943769b1cd10778ffd611118498458
Too Lee <too.lee@lmco.com>
2016-09-21 12:07:12 -0400

    Add non-explicit ctor test

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+class A
+{
+  public:
+   A(int i, int j){}
+};
+
+void f(A a)
+{
+  cout << "in f" << endl;
+}
 int main(int argc, const char *argv[])
 {
-  
+  f({1,2}); // does compile without explicit only
+
   return 0;
 }
