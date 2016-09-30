2f049040f12b367629fb8c25bb4915c637144cf7
Too Lee <too.lee@lmco.com>
2016-09-30 15:10:39 -0400

    add function_traits struct

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <tuple>
 
-using namespace std;
+template <typename T>
+struct function_traits
+    : public function_traits<decltype(&T::operator())>
+{};
+// For generic types, directly use the result of the signature of its 'operator()'
 
-int main(int argc, const char *argv[])
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits<ReturnType(ClassType::*)(Args...) const>
+// we specialize for pointers to member function
 {
-  
-  return 0;
+    enum { arity = sizeof...(Args) };
+    // arity is the number of arguments.
+
+    typedef ReturnType result_type;
+
+    template <size_t i>
+    struct arg
+    {
+        typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+        // the i-th argument is equivalent to the i-th tuple element of a tuple
+        // composed of those arguments.
+    };
+};
+
+// test code below:
+int main()
+{
+    auto lambda = [](int i) { return long(i*10); };
+
+    typedef function_traits<decltype(lambda)> traits;
+
+    static_assert(std::is_same<long, traits::result_type>::value, "err");
+    static_assert(std::is_same<int, traits::arg<0>::type>::value, "err");
+
+    return 0;
 }
