2aea79764a5d2ea71f8209b3018967c19b8cc373
Too Lee <too.lee@lmco.com>
2016-10-20 11:48:40 -0400

    add

---
+++ foo.hpp
@@ -0,0 +1,10 @@
+#ifndef FOO_HPP
+#define FOO_HPP
+
+class Foo {
+  public:
+    void def_twice();
+
+};
+
+#endif
---
+++ foo2.cpp
@@ -0,0 +1,7 @@
+#include "foo.hpp"
+#include <iostream>
+using namespace std;
+void Foo::def_twice()
+{
+  cout << "foo2.cpp" << endl;
+}
