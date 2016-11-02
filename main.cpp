e126ae139972be3970b4156087bfbed75f0544f1
Too Lee <too.lee@lmco.com>
2016-11-02 10:39:03 -0400

    Add example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <array>
 using namespace std;
+
+array<int,64> buffer;
 
 int main(int argc, const char *argv[])
 {
-  
+  char* c = reinterpret_cast<char*>(&buffer[0]);
+  for(unsigned int i=0; i < 64; i++)
+  {
+    printf("%02X", *(c+i));
+      if ( (i+1) % 8 == 0 )
+        printf("\n");
+      else
+        printf(" ");
+  }
   return 0;
 }
