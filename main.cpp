bd5b9dc8cae7775c1ae4b50577e76c0b6d7e16c8
Too Lee <too.lee@lmco.com>
2016-11-22 07:24:58 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+struct Foo
+{
+  int32_t  i1 : 8;
+  int32_t     : 8;
+};
+
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
-  
+  cout << "int sizeof " << sizeof(int32_t) << endl;
+  cout << "uint sizeof " << sizeof(uint32_t) << endl;
+  cout << sizeof(Foo) << endl;
+
   return 0;
 }
