bd2a71ab83374846716d3a819ea4cdda9314b9f4
Too Lee <too.lee@lmco.com>
2017-04-05 21:56:47 -0400

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <boost/lockfree/spsc_queue.hpp>
+#include <thread>
+#include <chrono>
+#include <boost/lockfree/stack.hpp>
 
 using namespace std;
-boost::lockfree::spsc_queue<int, boost::lockfree::capacity<10>> q;
+boost::lockfree::stack<int, boost::lockfree::capacity<3>> q;
 
 void push(int i)
 {
@@ -20,71 +22,48 @@ void pop(int& tmp)
   else
     cout << "Fail pop " << endl;
 }
-template<unsigned long N>
-void pop(std::array<int, N>& arr)
-{
-  int size = q.pop(&arr[0]);
-  if (size > 0)
-  {
-    for (unsigned int i=0; i<N; i++)
-    {
-      cout << "Done pop: size " << size << " arr[" << i << "] = " << arr[i] << endl;
-    }
-  }
-  else
-  {
-    cout << "Fail pop: nothing to pop" << endl;
-  }
-}
 
 void test1()
 {
   cout << "Running test1" << endl;
   push(1);
   push(2);
+  push(3);
+  push(4);
 
   int tmp;
   pop(tmp);
 
-  push(3);
+  push(5);
 
   pop(tmp);
   pop(tmp);
+
+  pop(tmp); // should fail
 }
+
+void thread_push() {
+  for( int i=0; i<10; i++ ){
+    push(i);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+void thread_pop() {
+  for( int i=0; i<10; i++ ){
+    int tmp;
+    pop(tmp);
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+  }
+}
+
 void test2()
 {
-  cout << "Running test2" << endl;
-  std::array<int, 10> arr;
+  std::thread t(thread_push);
+  std::thread t2(thread_pop);
 
-  push(1);
-  push(2);
-  push(3);
-  push(4);
-  push(5);
-  push(6);
-  push(7);
-  push(8);
-  push(9);
-  push(10); // cannot pushed even though the queue size is set to 10
-  push(11);
-  push(12);
-  pop(arr);
-
-  push(13); // push return true (bug)
-  push(14);
-  pop(arr); // popped array showed 14, 2, 3, ...  // missing 13
-
-  push(15);
-  push(16);
-  pop(arr);
-
-  push(17);
-  push(18);
-  pop(arr);
-
-  push(19);
-  push(20);
-  pop(arr);
+  t.join();
+  t2.join();
 }
 int main(int argc, const char *argv[])
 {
