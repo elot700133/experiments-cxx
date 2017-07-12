30d6daed126798e50fbe19ad52e724bcc6b8bb5e
Too Lee <too.lee@lmco.com>
2017-07-12 15:12:34 -0400

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-  
+  uint16_t y = 0;
+  char* p = (char*)(&y);
+  *p = 0x01;
+  *(p+1) = 0xe0;
+  printf("y = %d\n", y);
   return 0;
 }
