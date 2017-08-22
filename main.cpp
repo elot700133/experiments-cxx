0b88cbaf3ab3caab64f492b463f97e175cbcc2ae
Too Lee <too.lee@lmco.com>
2017-08-22 12:39:20 -0400

    update

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
+
 int main(int argc, const char *argv[])
 {
-  uint16_t y = 0;
-  char* p = (char*)(&y);
-  *p = 0x01;
-  *(p+1) = 0xe0;
-  printf("y = %d\n", y);
+  uint32_t col1[10];
+  uint32_t col2[10];
+  uint32_t combined[10];
+  for ( int32_t i=0; i<10; i++)
+  {
+    col1[i] = i;
+    col2[i] = i;
+  }
+
+
+
   return 0;
 }
