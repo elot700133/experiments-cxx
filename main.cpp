a12621da2b59bb8ca277e5171c634bc4652e9824
Too Lee <too.lee@lmco.com>
2017-08-28 09:10:15 -0400

    Refresh output and take input simultaneously

--- main.cpp
+++ main.cpp
@@ -1,83 +1,49 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
-using namespace std;
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
-int main(int argc, const char *argv[])
-{
-  WINDOW *my_win;
-  int startx, starty, width, height;
-  int ch;
+int main() {
+    char buf[100] = {0}, *s = buf;
+    int ch, cnt = 0, n = 1;
+    WINDOW *w;
 
-  initscr();                    /* Start curses mode            */
-  cbreak();                     /* Line buffering disabled, Pass on
-           * everty thing to me                 */
-  keypad(stdscr, TRUE);         /* I need that nifty F1         */
-
-  height = 3;
-  width = 10;
-  starty = (LINES - height) / 2;        /* Calculating for a center placement */
-  startx = (COLS - width) / 2;  /* of the window                */
-  printw("Press F1 to exit");
-  refresh();
-  my_win = create_newwin(height, width, starty, startx);
-
-  while((ch = getch()) != KEY_F(1))
-  {     switch(ch)
-    {   case KEY_LEFT:
-        destroy_win(my_win);
-        my_win = create_newwin(height, width, starty,--startx);
-        break;
-      case KEY_RIGHT:
-        destroy_win(my_win);
-        my_win = create_newwin(height, width, starty,++startx);
-        break;
-      case KEY_UP:
-        destroy_win(my_win);
-        my_win = create_newwin(height, width, --starty,startx);
-        break;
-      case KEY_DOWN:
-        destroy_win(my_win);
-        my_win = create_newwin(height, width, ++starty,startx);
-        break;
+    if ((w = initscr()) == NULL) {
+        fprintf(stderr, "Error: initscr()\n");
+        exit(EXIT_FAILURE);
     }
-  }
+    keypad(stdscr, TRUE);
+    noecho();
+    cbreak();      // disable line-buffering
+    timeout(100);  // wait 100 milliseconds for input
 
-  endwin();                     /* End curses mode                */
-  return 0;
-}
+    while (n != 0) {
+        erase();
+        mvprintw(0, 0, "cnt: %d", cnt++);
+        mvprintw(1, 0, "buf: %s", buf);
+        mvprintw(2, 0, "int: %d", n);
+        refresh();
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{       WINDOW *local_win;
+        // getch (with cbreak and timeout as above)
+        // waits 100ms and returns ERR if it doesn't read anything.
+        if ((ch = getch()) != ERR) {
+            if (ch == '\n') {
+                *s = 0;
+                sscanf(buf, "%d", &n);
+                s = buf;
+                *s = 0;
+            }
+            else if (ch == KEY_BACKSPACE) {
+                if (s > buf)
+                    *--s = 0;
+            }
+            else if (s - buf < (long)sizeof buf - 1) {
+                *s++ = ch;
+                *s = 0;
+            }
+        }
+    }
 
-  local_win = newwin(height, width, starty, startx);
-  box(local_win, 0 , 0);                /* 0, 0 gives default characters
-           * for the vertical and horizontal
-           * lines                      */
-  wrefresh(local_win);          /* Show that box                */
-
-  return local_win;
-}
-
-void destroy_win(WINDOW *local_win)
-{
-  /* box(local_win, ' ', ' '); : This won't produce the desired
-   * result of erasing the window. It will leave it's four corners
-   * and so an ugly remnant of window.
-   */
-  wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-  /* The parameters taken are
-   * 1. win: the window on which to operate
-   * 2. ls: character to be used for the left side of the window
-   * 3. rs: character to be used for the right side of the window
-   * 4. ts: character to be used for the top side of the window
-   * 5. bs: character to be used for the bottom side of the window
-   * 6. tl: character to be used for the top left corner of the window
-   * 7. tr: character to be used for the top right corner of the window
-   * 8. bl: character to be used for the bottom left corner of the window
-   * 9. br: character to be used for the bottom right corner of the window
-   */
-  wrefresh(local_win);
-  delwin(local_win);
+    delwin(w);
+    endwin();
+    return 0;
 }
