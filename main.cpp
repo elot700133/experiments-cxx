d0a4fba1433669acd0f0976d3729e334400c0a25
Too Lee <too.lee@lmco.com>
2018-02-13 17:38:59 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+template<class T>
+struct is_optimizied
+{
+  static const bool value = false;
+};
+
+template<>
+struct is_optimizied<int>
+{
+  static const bool value = true;
+};
+
 int main(int argc, const char *argv[])
 {
-  
+  std::cout<<is_optimizied<int>::value<<std::endl;
+
   return 0;
 }
