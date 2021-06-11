280e1b7448fbbe38784e85081798a805bae5b579
toolee <toolee@users.noreply.github.com>
2021-06-10 23:54:32 -0500

    Update linked-list.cpp

--- linked-list.cpp
+++ linked-list.cpp
@@ -27,3 +27,23 @@ ListNode* reverseList(ListNode* head) {
     }
     return prev;
 }
+
+// template for using two pointers techniques
+// can find cycling link list
+bool twoPointersTemplateExample{
+    // Initialize slow & fast pointers
+    ListNode* slow = head;
+    ListNode* fast = head;
+    /**
+     * Change this condition to fit specific problem.
+     * Attention: remember to avoid null-pointer error
+     **/
+    while (slow && fast && fast->next) {
+        slow = slow->next;          // move slow pointer one step each time
+        fast = fast->next->next;    // move fast pointer two steps each time
+        if (slow == fast) {         // change this condition to fit specific problem
+            return true;
+        }
+    }
+    return false;   // change return value to fit specific problem
+}
