956ed1ec113da6ddfd06144261dce1c8f8095495
toolee <toolee@users.noreply.github.com>
2021-06-29 22:46:07 -0500

    Update binary-tree.cpp

--- binary-tree.cpp
+++ binary-tree.cpp
@@ -119,3 +119,15 @@ public:
         // traverse based on level
     }
 };
+
+//=======================================================
+void maximum_depth(TreeNode* root, int depth) {
+    if (!root) {
+        return;
+    }
+    if (!root->left && !root->right) {
+        answer = max(answer, depth);
+    }
+    maximum_depth(root->left, depth + 1);
+    maximum_depth(root->right, depth + 1);
+}
