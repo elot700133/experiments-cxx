4cb37e4a4c3ea30b7e26d8cb56d3374e587f549a
toolee <toolee@users.noreply.github.com>
2021-07-01 08:17:11 -0500

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,25 +6,30 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         // two pointer approach
-        string ans;
+        string ans = "";
         int anchor = 0;
         while(anchor < s.size()) {
             // on anchor
             {
                 int i1 = anchor;
                 int i2 = anchor;
+                int start = 0;
+                int max_len = 0;
                 string tmp_ans;
                 while(i1 > -1 && i2 < s.size()) {
                     if (s[i1] == s[i2]) {
-                        tmp_ans = s.substr(i1, i2-i1+1);
+                        //printf("1: %d %d %c %c\n",i1,i2,s[i1],s[i2]);
+                        start = i1;
+                        max_len = i2-i1+1;
+                        i1--; i2++;
                     }
                     else
                         break;
-                    i1--; i2++;
                 }
-                //printf("1 %s\n",tmp_ans.c_str());
-                if (ans.size() < tmp_ans.size()) {
-                    ans = tmp_ans;
+
+                if(ans.size() < max_len )  {
+                    //printf("%d\n",max_len);
+                    ans = s.substr(start, max_len);
                 }
             }
             
@@ -32,23 +37,53 @@ public:
             {
                 int i1 = anchor;
                 int i2 = anchor+1;
+                int start = 0;
+                int max_len = 0;
                 string tmp_ans;
                 while(i1 > -1 && i2 < s.size()) {
                     if (s[i1] == s[i2]) {
-                        //printf("2: %d %d %c %c\n",i1,i2,s[i1],s[i2]);
-                        tmp_ans = s.substr(i1, i2-i1+1);
+                        //printf("1: %d %d %c %c\n",i1,i2,s[i1],s[i2]);
+                        start = i1;
+                        max_len = i2-i1+1;
+                        i1--; i2++;
                     }
                     else
                         break;
-                    i1--; i2++;
                 }
-                //printf("2 %s\n",tmp_ans.c_str());
-                if (ans.size() < tmp_ans.size()) {
-                    ans = tmp_ans;
-                }    
+
+                if(ans.size() < max_len )  {
+                    //printf("%d\n",max_len);
+                    ans = s.substr(start, max_len);
+                }
             }
-            anchor++;
+            anchor++; 
         }
         return ans;
     }
+    // others' best solution
+    string longestPalindrome1(string s) {
+        if (s.empty()) return "";
+        if (s.size() == 1) return s;
+        int min_start = 0, max_len = 1;
+        for (int i = 0; i < s.size();) {
+            printf("anchor %d\n",i);
+          if (s.size() - i <= max_len / 2) break;
+          int j = i, k = i;
+          while (k < s.size()-1 && s[k+1] == s[k]){               // <========= don't understand this line
+              printf("%d %d %c %c\n",k+1, k, s[k+1], s[k]);
+              ++k; // Skip duplicate characters.
+          }
+          i = k+1;
+          while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) {
+              printf("2: %d %d %c %c\n",k+1, j-1, s[k+1], s[j-1]);
+              ++k; --j; 
+          } // Expand.
+          int new_len = k - j + 1;
+          if (new_len > max_len) { min_start = j; max_len = new_len; }
+        }
+        return s.substr(min_start, max_len);
+    }
 };
+
+
+
