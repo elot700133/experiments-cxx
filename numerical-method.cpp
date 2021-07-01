f7f690400a09068e411f415cfaa2dacbeb000ddc
toolee <toolee@users.noreply.github.com>
2021-07-01 00:15:11 -0500

    Update numerical-method.cpp

--- numerical-method.cpp
+++ numerical-method.cpp
@@ -48,3 +48,40 @@ int addOvf(int* result, int a, int b)
         }
         return ans;
     }
+
+//=======================================================================================================================
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // map of target - nums : index
+        // [7,2,3,4] 9
+        // map 7:0, 2:1, 3:2, 4:3
+        
+        // look for 9-7 = 2;
+        // map.find(2)->second -> 1;
+       
+        vector<int> v;
+        map<int,int> index;
+        for(int i=0; i<nums.size(); i++) {
+            index[nums[i]] = i;
+        }
+        
+        for(int i=0; i<nums.size(); i++) {
+            auto iter = index.find(target - nums[i]);
+            if(iter != index.end()) {
+                if ( i != iter->second ) {
+                  v.push_back(i);
+                  v.push_back(iter->second);
+                  return v;
+                }
+            }
+        }
+        return v;
+    }
+};
