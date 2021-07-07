014712d24759e30e281749351e9b78bb222e469d
toolee <toolee@users.noreply.github.com>
2021-07-06 22:38:05 -0500

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,101 @@
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// basic operations
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+//=======================================
+// insert at beginning of array
+for (int i = 3; i >= 0; i--) {
+    intArray[i + 1] = intArray[i];
+}
+
+// Now that we have created space for the new element,
+// we can insert it at the beginning.
+intArray[0] = 20;
+
+//=========================================
+// insert at anywhere
+// Say we want to insert the element at index 2.
+// First, we will have to create space for the new element.
+for (int i = 4; i >= 2; i--)
+{
+    // Shift each element one position to the right.
+    intArray[i + 1] = intArray[i];
+}
+
+// Now that we have created space for the new element,
+// we can insert it at the required index.
+intArray[2] = 30;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// questions
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+//=============================================
+// duplicate zeros
+//Input: [1,0,2,3,0,4,5,0]
+//Output: null
+//Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i=0; i<arr.size(); ++i) {
+            if ( arr[i] == 0) {
+                insert(i,0,arr);
+                ++i;
+            }
+        }
+    }
+    
+    void insert(int index, int value, vector<int>& arr) {
+        for(int i=arr.size()-2; i>=index; --i) {
+            arr[i+1] = arr[i];
+        }
+        arr[index] = value;
+    }
+};
+
+//=====================================================
+// merge sorted array
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and
+// two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+//The final sorted array should not be returned by the function, but instead be stored inside
+//the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements
+//denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+//The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0;
+        vector<int> ans(m+n);
+        int k=0;
+        while(j<n || i<m) {
+            if ( i<m && j<n && nums1[i] < nums2[j] ) {
+                ans[k++] = nums1[i];
+                i++;
+            }
+            else if ( j<n ) {
+                ans[k++] = nums2[j];
+                j++;
+            }
+            else if ( i<m) {
+                ans[k++] = nums1[i];
+                i++;
+            }
+        }
+        nums1 = ans;
+    }
+    
+    void insert(int index, int value, vector<int>& arr) {
+        for(int i=arr.size()-2; i>=index;--i) {
+            arr[i+1] = arr[i];
+        }
+        arr[index] = value;
+    }
+};
