83c4d6da1dad61af3dc03c4f01d1510742dd8763
toolee <toolee@users.noreply.github.com>
2021-07-07 23:52:06 -0500

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,7 +1,10 @@
 //=============================================
 // iterative 
 //
+//---------------------------------------------
 // using stack
+// O(n)
+// O(n)
 void reverse_string(string &str)
 {
    stack<char> st;
@@ -12,4 +15,24 @@ void reverse_string(string &str)
        str[i] = st.top();
        st.pop();
    }      
+}
+//---------------------------------------------
+// using swap
+// O(n)
+// O(1)
+void reverse_string(string& str) {
+   int n = str.length();
+   for(int i=0; i<n/2; i++) {
+      swap(str[i],str[n-1-i]);
+   }
+}
+//---------------------------------------------
+// two pointers
+// O(n)
+// O(1)
+void reverse_string(string& str) {
+   int n = str.length();
+   for(int i=0, int j = n-1; i<j; i++, j--) {
+      swap(str[i],str[j]);
+   }
 }
